e7cb30507803a0d8aafbad09340e572b5043fefe
c3c1l1a <cesswangumukima@gmail.com>
2022-09-01 22:36:11 +0800

    Add any and filter in the enumerable

--- MyEnumerable.rb
+++ MyEnumerable.rb
@@ -5,4 +5,19 @@ module MyEnumerable
 		end
 		true
 	end
+
+	def any?
+		each do |y|
+			return true if yields y
+		end 
+		false
+	end 
+
+	def filter
+		newList = []
+		each do |y|
+			newList<<y if yields y
+		end
+		return newList
+	end 
 end
--- MyList.rb
+++ MyList.rb
@@ -11,8 +11,17 @@ class MyList
 		@list.all? {|e| e < 5}
 	end
 
+	def any?
+		@list.any? {|e| e == 2}
+	end 
+
+	def filter
+		@list.filter {|e| e.odd?} 
+	end
 end 
 
 myList = MyList.new(1, 2, 3, 4)
 p myList.all? 
+p myList.any?
+p myList.filter
 
