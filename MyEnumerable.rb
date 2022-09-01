c0f81156124f9069984c69c02f9a958c427d5d87
Abby Nyakara <81410040+AbbyNyakara@users.noreply.github.com>
2022-09-01 18:11:31 +0300

    Update MyEnumerable.rb

--- MyEnumerable.rb
+++ MyEnumerable.rb
@@ -1,23 +1,23 @@
 module MyEnumerable
 	def all?
-	  each do |y|
-	    return false unless yields y
-	  end
-	  true
+		each do |y|
+			return false unless yields y
+		end
+		true
 	end
 
-def any?
-	each do |y|
-		return true if yields y
+	def any?
+		each do |y|
+			return true if yields y
+		end 
+		false
 	end 
-	false
-end 
 
-def filter
-	newList = []
-	each do |y|
-		newList<<y if yields y
-	end
-	return newList
-end 
+	def filter
+		newList = []
+		each do |y|
+			newList<<y if yields y
+		end
+		return newList
+	end 
 end
