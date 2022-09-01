20a60745b07e29d404e28eeb9ffa589525254ab8
Abby Nyakara <81410040+AbbyNyakara@users.noreply.github.com>
2022-09-01 18:18:20 +0300

    Update MyList.rb

--- MyList.rb
+++ MyList.rb
@@ -1,27 +1,13 @@
 require './MyEnumerable.rb'
 
 class MyList
-	include MyEnumerable
-	
-	def initialize(*args)
-		@list = args
-	end 
+  def initialize(*args)
+    @list = args
+  end
 
-	def all? 
-		@list.all? {|e| e < 5}
-	end
+  include MyEnumerable
 
-	def any?
-		@list.any? {|e| e == 2}
-	end 
-
-	def filter
-		@list.filter {|e| e.odd?} 
-	end
-end 
-
-myList = MyList.new(1, 2, 3, 4)
-p myList.all? 
-p myList.any?
-p myList.filter
-
+  def each(&block)
+    @list.each { |y| block.call(y) }
+  end
+end
